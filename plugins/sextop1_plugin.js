de165fe10fc99de74ee1273e7e38c1d64b6e0a2c
youngbi <32502366+youngbi@users.noreply.github.com>
2026-04-27 22:25:25 +0700

    Update sextop1_plugin.js

--- plugins/sextop1_plugin.js
+++ plugins/sextop1_plugin.js
@@ -12,7 +12,6 @@ function getManifest() {
         "isEnabled": true,
         "isAdult": true,
         "type": "VIDEO",
-        "playerType": "embed",
         "layoutType": "HORIZONTAL"
     });
 }
